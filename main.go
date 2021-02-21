bab21d2dda318e0e0bd9fe2755893a424cf49d76
didzl <delpiero1987@naver.com>
2021-02-21 21:20:22 +0900

    golang study first

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+func canIDrink(age int) bool {
+	if koreanAge:= age+2;  koreanAge <18 {
+		return false
+	} 
+	return true
+}
+
+
+//start point
+func main() {
+	fmt.Println(canIDrink(16))
+}
